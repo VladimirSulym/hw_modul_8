ccf971784a25a31ad735eb486e38e10c0c4f3588
Vladimir Sulym <vormagic@yandex.ru>
2025-02-28 19:41:40 +0300

    An lms application and models have been created for it

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'users',
+    'lms',
 ]
 
 REST_FRAMEWORK = {
---
+++ lms/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Сourse(models.Model):
+    title = models.CharField(verbose_name='Название курса', max_length=255)
+    avatar = models.ImageField(verbose_name='Превью', upload_to='lms/course', blank=True, null=True)
+    city = models.CharField(max_length=255, unique=True, verbose_name='Город', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+        ordering = ['title', ]
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Сourse, verbose_name='Курс', on_delete=models.PROTECT, related_name='lessons')
+    title = models.CharField(verbose_name='Название урока', max_length=255)
+    description = models.TextField(verbose_name='Описание')
+    avatar = models.ImageField(verbose_name='Превью', upload_to='lms/lesson/img', blank=True, null=True)
+    video = models.FileField(upload_to='lms/lesson/video', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+        ordering = ['title', 'course', ]
+
+    def __str__(self):
+        return self.title
