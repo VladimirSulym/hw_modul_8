4bffc96b84b5c3541586d2900d80e30672613122
Vladimir Sulym <vormagic@yandex.ru>
2025-02-28 19:14:41 +0300

    The user model has been created

--- users/models.py
+++ users/models.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Электронная почта')
+    phone = models.CharField(verbose_name='Мобильный телефон', max_length=30, blank=True)
+    avatar = models.ImageField(verbose_name='Фотография', upload_to='employees/', blank=True, null=True)
+    city = models.CharField(max_length=255, unique=True, verbose_name='Город')
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+        ordering = ['email', ]
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
