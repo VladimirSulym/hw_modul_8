d7bc454f3e3c6a734edf6d0b861566356f6cafcc
Vladimir Sulym <vormagic@yandex.ru>
2025-04-30 00:01:05 +0300

    Add .dockerignore and re-enable database configurations
    Created a .dockerignore file to exclude unnecessary files in Docker setup. Re-enabled PostgreSQL configurations in GitHub Actions and commented out test database settings in Django to streamline handling of environments.

--- config/settings.py
+++ config/settings.py
@@ -193,8 +193,8 @@ CELERY_BEAT_SCHEDULE = {
     },
 }
 
-if 'test' in sys.argv:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'test_db.sqlite3'
-    }
+# if 'test' in sys.argv:
+#     DATABASES['default'] = {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'test_db.sqlite3'
+#     }
